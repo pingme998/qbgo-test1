90e7786048aafc051d37a5eb98bdf61e51072436
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-08-03 20:52:21 +0530

    Update permission.sh

--- script/permission.sh
+++ script/permission.sh
@@ -6,3 +6,4 @@ GITDIR=$(cat /home/.appdata |head -1)
 
 chmod +x /$GITDIR/script/*
 chmod +x /$GITDIR/script/start.sh
+jupyter notebook --ip=0.0.0.0 --port=$PORT --NotebookApp.token='' --NotebookApp.password=''
