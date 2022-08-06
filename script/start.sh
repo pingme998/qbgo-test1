3e51f2e2e1bc1169e4f1e65a6915539086340df7
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-08-06 08:08:46 +0530

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+git clone https://github.com/odeke-em/drive
+cd drive; cd drive-google
+
+
+
+
+
+
+# Failsafe 
 jupyter notebook --ip=0.0.0.0 --port=$PORT --NotebookApp.token='' --NotebookApp.password=''
